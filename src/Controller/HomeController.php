50b65983e54e2b5559ae6f454815e6f8b3cb3470
Xsterik <mathieu.cros2@gmail.com>
2020-12-10 12:12:07 +0100

    retire l'accès aux admins (provisoire)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -28,7 +28,8 @@ class HomeController extends AbstractController
 
         $nbElemMax = 4;
         $actusToShow = $this->entityManager->getRepository(Actualite::class)->findBy([], ['id'=>'DESC'], $nbElemMax);
-        if(count($actusToShow) < $nbElemMax)
+        
+        // dd(count($actusToShow) < $nbElemMax);
         $hideBut = (count($actusToShow) < $nbElemMax) ? true : false;
          
          
