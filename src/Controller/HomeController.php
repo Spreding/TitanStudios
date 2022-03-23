e06ab3b49cac3a935c4ba6ed009e7fd3c3c751ad
Tanguy <tanguy.augras@gmail.com>
2022-03-23 10:16:36 +0100

    git for deployment

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,75 +13,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
     /**
      * @Route("/", name="home")
      */
     public function index(Request $request): Response
     {
-         $realisationsHighlight = $this->entityManager->getRepository(Realisations::class)->findRealisationsHighlighted();
 
-        $nbElemMax = 4;
-        $actusToShow = $this->entityManager->getRepository(Actualite::class)->findBy([], ['id'=>'DESC'], $nbElemMax);
-        
-
-        $hideBut = (count($actusToShow) < $nbElemMax) ? true : false;
-         
-         
-    if ($request->isXmlHttpRequest()){
-        $jsonData =  array();
-
-        $firstactu = $this->entityManager->getRepository(Actualite::class)->findBy([], ['id'=>'ASC'], 1);
-         $search = $request->request->all();
-
-        $offset = $search['data'] * $nbElemMax;
-        $actusToAdd = $this->entityManager->getRepository(Actualite::class)->findBy([], ['id'=>'DESC'], $nbElemMax, $offset);
-
-        $canFetchMore = in_array($firstactu[0], $actusToAdd);
-          for ($i=0; $i < count($actusToAdd); $i++) { 
-            $temp = array(
-                'title' => $actusToAdd[$i]->getTitle(),
-                'description' => $actusToAdd[$i]->getdescription(),
-                'image' => $actusToAdd[$i]->getImage(),
-                'slug' => $actusToAdd[$i]->getSlug(),
-                'last' => $canFetchMore,
-            );
-
-            $jsonData[$i] = $temp;
-          } 
-          
-          
-        return new JsonResponse($jsonData);
-    }
-
-        return $this->render('home/index.html.twig',[
-            'highlight' => $realisationsHighlight,
-            'actus' => $actusToShow,
-            'isButHide'=> $hideBut,
-        ]);
-    }
-
-
-    /**
-     * @Route("/actualite/{slug}", name="actualite")
-     */
-    public function actualite($slug): Response
-    {
-        $realisationsHighlight = $this->entityManager->getRepository(Realisations::class)->findRealisationsHighlighted();
-        $actualite = $this->entityManager->getRepository(Actualite::class)->findOneBySlug($slug);
-
-        if(!$actualite){
-            return $this->redirectToRoute('home');
-        }
-
-        return $this->render('home/actu.html.twig', [
-            'highlight' => $realisationsHighlight,
-            'actu' =>$actualite,
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
